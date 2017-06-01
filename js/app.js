0c7c8daeb0974eb1bd25c80d126d8f803640e7a7
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-01 18:04:07 +0100

    first  controller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var app01 = angular.module("app01", []);
+var app01 = angular.module("app", []);
+
+app01.controller("appController", function(){
+    this.name = "This is the first controller";
+});
