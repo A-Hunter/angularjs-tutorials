1b1c73764819ab02ab44d17ca4c90dd98fc9161f
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-01 19:15:20 +0100

    ng-hide and ng-show

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var app = angular.module("app", []);
 
 app.controller("appController1", function(){
     this.name = "This is the first controller";
+    this.showMe = true;
+    this.hideMe = true;
 });
 
 app.controller("appController2", function(){
