546eff1f4f461e7ae965e1be1e312061641dc567
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-02 02:54:48 +0100

    ng-repeat example

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 var app = angular.module("app", []);
 
 app.controller("appController1", function(){
+
+    // First Lesson : Oneway Data Binding
     this.name = "This is the first controller";
+
+    // Second Lesson : ng-hide and ng-show
     this.showMe = true;
     this.hideMe = true;
+
+    // Third Lesson : ng-repeat
+    this.text = "ABC";
+    this.tab = ['i','j','k'];
+    this.characters = [{firstName:"Itachi", lastName:"Uchiha", age:"25", occupation:"Shinobi", birthDate:"1989-01-01"},
+                       {firstName:"Sishui", lastName:"Uchiha", age:"28", occupation:"Shinobi", birthDate:"1986-02-02"},
+                       {firstName:"Netero", lastName:"Isaac", age:"125", occupation:"Hunter", birthDate:"1889-03-03"}]
 });
 
 app.controller("appController2", function(){
