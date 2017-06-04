57d82970cd1070a9746d683692fef6ca54f96468
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-04 15:46:25 +0100

    basic directive

---
+++ part_3/js/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('app',[]);
+
+app.controller("appController", function(){
+    this.characters = [{
+        firstName:"Netero", lastName:"Isaac", email:"issac.netero@gmail.com",age:"125",occupation:"Hunter",birthDate:"1889-01-01"
+    }];
+
+    this.submitForm = function(){
+        this.characters.push({firstName:this.Object.firstName, lastName:this.Object.lastName,email:this.Object.email,age:this.Object.age,occupation:this.Object.occupation,birthDate:this.Object.birthDate});
+
+        this.Object.firstName="";
+        this.Object.lastName="";
+        this.Object.email="";
+        this.Object.age="";
+        this.Object.occupation="";
+        this.Object.birthDate="";
+    }
+});
+
+app.directive("headerPage", function(){
+    return{
+        restrict:'E',
+        templateUrl:'attachments/header.html'
+    };
+});
+
+app.directive("bodyPage", function(){
+    return{
+        restrict: 'E',
+        templateUrl:'attachments/body.html'
+    }
+});
+
+app.directive("bodyPage", function(){
+    return{
+        restrict:'A',
+        templateUrl:'attachments/body.html'
+    }
+});
