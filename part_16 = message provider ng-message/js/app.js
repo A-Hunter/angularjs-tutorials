1d33114d01f75058aace37a8949e98886a447859
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-07 18:58:28 +0100

    message provider ng-messages / ng-message / ng-messages-multiple

---
+++ part_16 = message provider ng-message/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('app',["ngMessages"]);
+
+app.controller('appController', ['$scope','$http','$exceptionHandler','$log', function($scope, $http,$cookies,$log){
+
+        
+}]);
+
+
