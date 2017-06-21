62670904734e21815bc601202cecad4013e146e7
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-06-21 15:58:40 +0100

    registration enhancement

--- part_19 = Basic forms application with user registration and logging/js/app.js
+++ part_19 = Basic forms application with user registration and logging/js/app.js
@@ -4,4 +4,28 @@ app.config(function($routeProvider){
     $routeProvider.when('/register',{templateUrl:'views/register.html'})
                   .when('/login',{templateUrl:'views/login.html'})
                   .when('/',{templateUrl:'views/login.html'})
+});
+
+
+app.directive("hideMe", function($animate){
+    return function(scope,element,attrs){
+        scope.$watch(attrs.hideMe, function(newVal){
+            if(newVal){
+                $animate.addClass(element,"toggle");
+            }else{
+                $animate.removeClass(element,"toggle");
+            }
+        })
+    };
+});
+
+app.animation(".toggle",function(){
+    return{
+        addClass:function(element,className){
+           TweenMax.to(element,1,{opacity:0}); 
+        },
+        removeClass:function(element,className){
+            TweenMax.to(element,1,{opacity:1});
+        }
+    }
 })
--- part_19 = Basic forms application with user registration and logging/js/registerController.js
+++ part_19 = Basic forms application with user registration and logging/js/registerController.js
@@ -1,9 +1,18 @@
 app.controller("registrationController", ['$scope','userRegistration',function($scope,userRegistration){
    
+    $scope.isHidden = true;
+    $scope.message = "";
+    
     $scope.formSubmit = function(user){
         var result = userRegistration(user);
         result.then(function(output){
-            alert("Success");
+            $scope.isHidden = false;
+            $scope.message = "User saved successfully !";
+            
+            user.firstname = "";
+            user.lastname = "";
+            user.email = "";
+            user.password = "";
         })
     }
 }])
